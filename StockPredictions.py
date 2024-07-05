0bc556c5f250fef523d8b576066e25a6f26e53e2
BUBB134 <benfaulkner2005@icloud.com>
2024-07-05 18:53:53 +0100

    Data Visualisation

--- StockPredictions.py
+++ StockPredictions.py
@@ -48,6 +48,62 @@ if data_source == "alphavantage":
         df = pd.read_csv(file_to_save)
 
 else:   #loading from kaggle
-    df = pd.read_csv(os.path.join("Stocks","hpq.us.txt"), delimiter=",", usecols=['Date','Open','High','Low','Close'])
+    #change the path to the location of the dataset
+    df = pd.read_csv(os.path.join("Stocks","a.us.txt"), delimiter=",", usecols=['Date','Open','High','Low','Close'])
     print("Loaded from kaggle repo")
 
+# Sort DataFrame by date
+df = df.sort_values('Date')
+
+# Double check the result
+df.head()
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
+high_prices = df.loc[:,"High"].as_matrix()
+low_prices = df.loc[:,"Low"].as_matrix()
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
+
+#normalise data so that it falls between 0 and 1
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+train_data = train_data.reshape(-1)
+test_data = scaler.transform(test_data).reshape(-1)
+
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+    EMA = gamma*train_data[ti] + (1-gamma)*EMA
+    train_data[ti] = EMA
+
+all_mid_data = np.concatenate([train_data, test_data], axis=0)
+
+window_size = 100
+N = train_data.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size,N):
+    if pred_idx >= N:
+        date = dt.datetime.strptime(k, "%Y-%m-%d").date() + dt.timedelta(days=1)
+    else:
+        date = df.loc[pred_idx, "Date"]
